c5020a766320a80de9af0ecc9d53c0039838d1a7
PedroHenriqueDuart3 <158067527+PedroHenriqueDuart3@users.noreply.github.com>
2024-10-09 09:42:16 -0300

    Subindo Responsividade

---
+++ script.js
@@ -0,0 +1,16 @@
+var botaoPlay = document.getElementById("ButtonPlayStore");
+var botaoApp = document.getElementById("ButtonAppStore");
+var botaoMenu = document.getElementById("buttonAdm");
+
+botaoPlay.addEventListener("click", function () {
+    window.open("https://play.google.com", "_blank");
+});
+
+botaoApp.addEventListener("click", function () {
+    window.open("https://apple.com/app-store/", "_blank");
+});
+
+botaoMenu.addEventListener("click", function () {
+    window.location.replace("TelaLoginCadeado/telaLoginCadeado1.html");
+});
+
