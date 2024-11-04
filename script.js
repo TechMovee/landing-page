cff52b5fbfc2d0a7bea31c8624c5937628758b83
PedroHenriqueDuart3 <158067527+PedroHenriqueDuart3@users.noreply.github.com>
2024-11-04 10:38:29 -0300

    Commitando as finais

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-var botaoPlay = document.getElementById("ButtonPlayStore");
-var botaoApp = document.getElementById("ButtonAppStore");
+
+// Criando uma varíavel para o botão
 var botaoMenu = document.getElementById("buttonAdm");
-var botaoTrocarBackground = document.getElementById("changeBackgroundButton");
 
-botaoPlay.addEventListener("click", function () {
-    window.open("https://play.google.com", "_blank");
-});
 
-botaoApp.addEventListener("click", function () {
-    window.open("https://apple.com/app-store/", "_blank");
-});
+
+// Funcionalidade do botão
 
 botaoMenu.addEventListener("click", function () {
     window.location.replace("telaLoginCadeado1.html");
 });
-
-// botaoTrocarBackground.addEventListener("click", function () {
-    
-//     var currentURL = window.location.href;
-//     console.log("The current URL is: " + currentURL);
- 
-//     const fileName = window.location.href.split("/").pop();
-//     if (fileName === "index.html"){ 
-//        window.location.replace("indexEscuro.html");
-//     }else{
-//       window.location.replace("index.html");
-//     }
-   
-// });
 
 
 
